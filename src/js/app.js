328c41306a42cd878008aa5f2ab57f0f598c4e65
Li <mac@szcube-de-macbook-pro.local>
2019-02-22 01:02:26 +0800

    https://github.com/estelalee/project

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,16 @@ $(document).ready(function () {
     $('.carousel').slick({
         arrows: false,
         autoplay: true,
-        fade:true,
+        fade:false,
         autoplaySpeed:3000,
         speed:500,
     });
 });
+
+$('.explore__slide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows:false,
+    autoplay:false,
+  });
